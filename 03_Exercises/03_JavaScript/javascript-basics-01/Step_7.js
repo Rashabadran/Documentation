11421766da735e8009af6aa80bd4840a8342c567
rasha <badranrasha685@gmail.com>
2022-12-07 13:37:51 +0200

    Step7 done

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_7.js
@@ -0,0 +1,13 @@
+var shoe=prompt("Please enter your shoe size: ")
+var birth_date=prompt("Please enter your birth year: ")
+alert(myfunction())
+
+function myfunction(){
+var mul=shoe*2
+var add=mul+5
+var mul2=add*50
+var sub=mul2-birth_date
+var add2=sub+1766
+return add2;
+}
+
