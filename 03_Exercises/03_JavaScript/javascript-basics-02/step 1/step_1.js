ad7f75354b5fe6176e79831502cca1373c9ed73c
rasha <badranrasha685@gmail.com>
2022-12-08 09:21:23 +0200

    Step_1 J2 donee

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step_1.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step_1.js
@@ -1,2 +1,6 @@
-var x=document.querySelectorAll("image1");
-<img onmouseover="y" onmouseout="x" src="images/image1_2.jpg" alt="image2" width="100px" height="100px"></img>
+function setNewImage(){
+document.getElementById("image1").src="images/image1_2.jpg"
+}
+function SetOldIm(){
+    document.getElementById("image1").src="images/image1.jpg"
+}
