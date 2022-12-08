da6be1bdc586e6d8feb89f8cfb0dfbb3adbf2194
rasha <badranrasha685@gmail.com>
2022-12-08 12:28:06 +0200

    Step_2_3donee

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,8 @@
+
+function vis(){
+    var show=document.getElementById("texte").style.display='block' ;
+}
+
+function hid(){
+    var show=document.getElementById("texte").style.display = "none";
+}
