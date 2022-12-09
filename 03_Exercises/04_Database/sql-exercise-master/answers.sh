e02b664574bdb52124b540989e4e17ad021da18b
rasha <badranrasha685@gmail.com>
2022-12-09 21:08:06 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -8,4 +8,15 @@ UPDATE students SET Points = 500 where Name='Basma'
 
 UPDATE students SET Points = 150 where Name='Alex'
 
+CREATE TABLE graduate(
+ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name TEXT NOT NULL UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation TEXT
+);
+INSERT INTO graduate VALUES(1,'Layal',18,'F',350,'08/09/2018')
+
+DELETE FROM students WHERE Name='Layal';
 
