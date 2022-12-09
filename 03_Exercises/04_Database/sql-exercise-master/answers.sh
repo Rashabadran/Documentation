a6d32e0cf258fd0206068e59aabaa89e02e22e40
rasha <badranrasha685@gmail.com>
2022-12-09 13:43:16 +0200

    ans

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,11 @@
+select Name from students
+select *from students where Age>30
+select Name from students where Gender='F' and Age=30
+select Points from students where Name='Alex'
+INSERT INTO students VALUES(7,'Rasha',25,'F',400)
+
+UPDATE students SET Points = 500 where Name='Basma'
+
+UPDATE students SET Points = 150 where Name='Alex'
+
+
