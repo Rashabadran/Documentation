4657cfd61b5bbe3ad3a57c3c06436219c51b39e5
rasha <badranrasha685@gmail.com>
2022-12-10 23:25:28 +0200

    Joins

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -1,14 +1,14 @@
-select Name from students
-select *from students where Age>30
-select Name from students where Gender='F' and Age=30
-select Points from students where Name='Alex'
-INSERT INTO students VALUES(7,'Rasha',25,'F',400)
+1. select Name from students
+2. select * from students where Age>30
+3. select Name from students where Gender='F' and Age=30
+4. select Points from students where Name='Alex'
+5. INSERT INTO students VALUES(7,'Rasha',25,'F',400)
 
-UPDATE students SET Points = 500 where Name='Basma'
+6. UPDATE students SET Points = 500 where Name='Basma'
 
-UPDATE students SET Points = 150 where Name='Alex'
+7. UPDATE students SET Points = 150 where Name='Alex'
 
-CREATE TABLE graduate(
+10. CREATE TABLE graduate(
 ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 Name TEXT NOT NULL UNIQUE,
 Age INTEGER,
@@ -16,7 +16,26 @@ Gender TEXT,
 Points INTEGER,
 Graduation TEXT
 );
-INSERT INTO graduate VALUES(1,'Layal',18,'F',350,'08/09/2018')
 
-DELETE FROM students WHERE Name='Layal';
+INSERT INTO  graduates (Id,Name,Age,Gender,Points)
+SELECT Id,Name,Age,Gender,Points FROM students
+WHERE name="Layal";
 
+11. UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE name="Layal";
+
+
+12. DELETE FROM students WHERE Name='Layal';
+
+14. SELECT employees.name, employees.Company, companies.Date
+FROM (employees
+INNER JOIN companies ON employees.Company = companies.name);
+
+15. SELECT employees.name
+FROM (employees
+INNER JOIN companies ON employees.Company = companies.name AND companies.Date<2000);
+
+16. SELECT companies.Name
+FROM (companies
+INNER JOIN employees ON employees.Company = companies.name AND employees.Role='Graphic Designer');
