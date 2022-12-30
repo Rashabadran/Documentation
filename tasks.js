c03f73b7738e83673628bb5957e8a41da19de68d
Rasha <badranrasha685@gmail.com>
2022-12-30 13:59:58 +0200

    check/uncheck commands

--- tasks.js
+++ tasks.js
@@ -52,8 +52,18 @@ function onDataReceived(text) {
   else if(text.startsWith("remove")){
     remove(text);
   }
+  else if(text.startsWith("remove")){
+    remove(text);
+  }
   else if(text.startsWith("edit")){
     edit(text);
+  }
+  
+  else if(text.startsWith("check")){
+    check(text);
+  }
+  else if(text.startsWith("uncheck")){
+    uncheck(text);
   }
 
   else{
@@ -82,13 +92,48 @@ var list=["apple", "banana","potato","strawberry"];
  */
 function tasknum(list){
   for(var i=0; i<list.length;i++){
-    if(list[i]=="apple" || list[i]=="banana" || list[i]=="potato" || list[i]=="strawberry" ){
-      console.log(i + ") " + " [✓] " + list[i]);
-    }
-    else{
-    console.log(i + ") " + " [] " + list[i]);
-    }
+    console.log(i+1 + ") " +checkk[i+1] + list[i]);
+   
   }
+}
+
+
+const checkk=[];
+for(var i=0;i<=list.length;i++){
+  checkk[i+1]="[ ]";
+}
+/**
+ *  check list
+ *
+ * @returns {void}
+ */
+function check(item){
+  item=item.trim();
+  item1=item.substring(6);
+  if(item==="check"){
+    console.log("Error please select an item to uncheck");
+  }
+  else{
+  checkk[Number(item1)]= checkk[Number(item1)].replace("[ ]","[✓]");
+}
+}
+
+  
+
+/**
+ *  uncheck list
+ *
+ * @returns {void}
+ */
+function uncheck(item){
+  item=item.trim();
+  item1=item.substring(8);
+  if(item==="uncheck"){
+    console.log("Error please select an item to uncheck");
+  }
+  else{
+  checkk[Number(item1)]= checkk[Number(item1)].replace("[✓]","[ ]");
+}
 }
 /**
  *  add list
@@ -107,6 +152,7 @@ function addlist(item){
   }
   
   }
+  
 
   /**
  *  remove
@@ -114,50 +160,51 @@ function addlist(item){
  * @returns {void}
  */
 function remove(item){
-  item=item.trim();
-  
-   if(item==="remove 1"){
-    list.shift();
-    console.log(list);
-  }
-  
-   else if(item==="remove"){
-    list.pop();
-    console.log(list);
-  }
-  else if(item==="remove 2"){
-    list.splice(1,1);
-    console.log(list)
-  }
-  else{
-    console.log("This remove number doesn't exist");
-  }
+  if(item==="remove\n"){
+  list.pop();
+  console.log("We removed the last item for you");
   
   }
+    
+    else if(item.substring(7)-1 >= list.length){
+      console.log("Error! This index doesn't exist");
+    }
+    
+    else{
+      list.splice(item.substring(7)-1,1); 
+      console.log("We removed this item for you")}
+  }
+  
+ 
 
  /**
  *  edit
- *
+ * 
  * @returns {void}
  */
  function edit(item){
   item=item.trim();
   item1=item.substring(5).trim();
-   if(item1.startsWith("1")){
-    list.splice(0,1,item1.substring(1).trim());
-    console.log(list)
-  }
+  item2=item.slice(5,7)-1;
+  if(item==="edit"){
+    console.log("ERROR!You didn't specify the item");
   
-   else if(item1!=""){
+  }
+  else if(isNaN(item2)){
     list.splice(list.length-1,1,item1);
-    console.log(list)
-   }
+    console.log("Last item is editted");
+  }
+
+  else {
+    list.splice(item2,1,item1.substring(1).trim());
+    console.log("You edited it");
+  }
+
   
-  else{
-    console.log("Error! Enter what you want to edit");
   }
   
-  }
+  
+
 
 
 
@@ -183,8 +230,12 @@ function help(){
   console.log('If you want to view the list just write list')
   console.log('If you want to add on list just write add and the item you want to add for example add juice')
   console.log('If you want to remove the last item just remove')
-  console.log('If you want to remove the 1st item just write remove 1')
-  console.log('If you want to remove the second item just write remove 2')
+  console.log('If you want to remove the nth item in the list just write remove nth')
+  console.log('example If you want to remove the 3rd item just write remove 3')
+  console.log("If you want to replace the nth item write edit nth new item")
+  console.log("If you want to replace the last item write edit new item")
+  console.log("If you want to check an item write check and the number of item")
+  console.log("If you want to uncheck an item write uncheck and the number of item")
 }
 
 
@@ -198,5 +249,18 @@ function quit(){
   process.exit();
 }
 
+const fs=require ('fs');
+const { json, text } = require('stream/consumers');
+const saveData=(list)=>{
+  const finished =(error) =>{
+    if(error){
+      console.error(error);
+      return;
+    }
+  }
+  const jsonData=JSON.stringify(list);
+  fs.writeFile('database.json',jsonData,finished);
+}
+saveData(list,"database.json")
 // The following line starts the application
 startApp("Rasha Badran")
