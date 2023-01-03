482843e99e9a9b17bf2102aad8de5c85ed395e9f
Rasha <badranrasha685@gmail.com>
2023-01-03 10:21:22 +0200

    step_3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -3,5 +3,25 @@ const server = express()
 const port = 3000
 
 server.listen(port, () => {
-  console.log(`ok`)
+  console.log(`ok`);
+
 })
+server.use(express.json());
+server.post("/", (requestt, respondd) => {
+  respondd.send("okii");
+});
+
+
+server.get( '/test' ,(requestt,respondd) =>{
+    respondd.send({status:200, message:"ok"});
+});
+server.get( '/time' ,(requestt,respondd) =>{
+    const datee=new Date();
+    const time='${datee.getHours()}:${datee.getSeconds()}';
+    respondd.send({
+      status:200,
+      message:time
+    });
+});
+
+
