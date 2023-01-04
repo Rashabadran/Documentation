b75be6652959ab099977466d8247cb48e3c0adcd
Rasha <badranrasha685@gmail.com>
2023-01-04 21:12:12 +0200

    step-7

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,25 +1,29 @@
-var express = require('express')
-var server = express()
+const express = require('express')
 const port = 3000
+const server = express()
+
 
 server.listen(port, () => {
   console.log(`ok`);
 
 });
-
+server.get("/",(requestt,respondd)=>{
+  respondd.send('Hello');
+});
 server.get( '/test' ,(requestt,respondd) =>{
     respondd.send({status:200, message:"ok"});
 });
 server.get( '/time' ,(requestt,respondd) =>{
     const datee=new Date();
-    const time='${datee.getHours()}:${datee.getSeconds()}';
-    respondd.send({
+    const time=`${datee.getHours()}:${datee.getSeconds()}`;
+    const message2={
       status:200,
-      message:time
-    });
+      message:time};
+      respondd.send(message2);
+    
 });
 server.get('/hello/:id',(requestt,respondd)=>{
-const message2='Hello, ${requestt.params.id}';
+const message2=`Hello, ${requestt.params.id}`;
 respondd.send({
     status:200,
     message:message2
@@ -50,3 +54,95 @@ const movies = [
   { title: 'Brazil', year: 1985, rating: 8 },
   { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 ]
+
+server.get("/movies/create", (requestt, respondd) => {
+    
+});
+
+server.get("/movies/read", (requestt, respondd) => {
+    respondd.send({
+        status: 200, 
+        movie: movies
+    });
+});
+
+server.get("/movies/update", (requestt, respondd) => {
+
+});
+
+server.get("/movies/delete", (requestt, respondd) => {
+
+})
+server.get("/movies/read/by-date", (requestt, respondd) => {
+  const arr=[...movies];
+  arr.sort(function(a,b){
+    if(a.year<b.year){
+      return -1;
+    }
+    else if(a.year>b.year){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  })
+  respondd.send({
+      status: 200, 
+      data:arr
+  });
+});
+
+server.get("/movies/read/by-rating", (requestt, respondd) => {
+  const arr=[...movies];
+  arr.sort(function(a,b){
+    if(a.rating>b.rating){
+      return -1;
+    }
+    else if(a.rating>b.rating){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  })
+  respondd.send({
+      status: 200, 
+      data:arr
+  });
+});
+
+server.get("/movies/read/by-title", (requestt, respondd) => {
+  const arr=[...movies];
+  arr.sort(function(a,b){
+    if(a.title<b.title){
+      return -1;
+    }
+    else if(a.title>b.title){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  })
+  respondd.send({
+      status: 200, 
+      data:arr
+  });
+});
+
+server.get("/movies/read/id/:id",(requestt,respondd) => {
+if(requestt.params.id>=0 && requestt.params.id<movies.length){
+  message2={status:200, 
+  data:movies[requestt.params.id]}
+
+}
+else {
+  message2 = {
+      status: 400,
+      error: true,
+      message: `The movie ${requestt.params.id} does not exist`
+  }
+
+}
+respondd.send(message2);
+})
