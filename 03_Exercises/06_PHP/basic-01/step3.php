736540bc6833b0ea5b775df91c9ad7755b73e863
Rasha <badranrasha685@gmail.com>
2023-02-17 15:10:55 +0200

    step3

---
+++ 03_Exercises/06_PHP/basic-01/step3.php
@@ -0,0 +1,12 @@
+<?php
+$name='Rasha';
+$email='badranrasha685@gmail.com';
+
+//output
+echo "My name is ",$name , " and my email is ",$email;
+
+$height=3;
+$width=5;
+$area=$height*$width;
+echo "area= "+$area;
+?>
