ad751cf6c96a576171449b2d1425e8171b5ffc47
Rasha <badranrasha685@gmail.com>
2023-02-19 16:36:12 +0200

    step8

---
+++ 03_Exercises/06_PHP/basic-02/step8.php
@@ -0,0 +1,12 @@
+<?php
+function powerOfTwo($i){
+    echo($i);
+    if($i%2==0){
+        echo (" is power of 2");
+    }
+    else{
+        echo (" is not power of 2");
+    }
+}
+powerOfTwo(4)
+?>
