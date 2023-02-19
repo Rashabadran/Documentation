99c6eefe705b930fde582e77c07de73ed496b202
Rasha <badranrasha685@gmail.com>
2023-02-19 16:05:32 +0200

    step5

---
+++ 03_Exercises/06_PHP/basic-02/step5.php
@@ -0,0 +1,32 @@
+<?php
+function convert($i){
+echo $i;
+    switch ($i) {
+        case "zero":
+            echo " is equals 0 <br>";
+            break;
+        case "one":
+            echo " is equals 1<br>";
+            break;
+        case "two":
+            echo " is equals 2<br>";
+            break;
+        case "three":
+            echo " is equals 3<br>";
+            break;
+        case "four":
+            echo " is equals 4<br>";
+            break;
+        case "five":
+            echo " is equals 5<br>";
+            break;
+    }
+}
+ convert("zero");
+ convert("one");
+ convert("two");
+ convert("three");
+ convert("four");
+ convert("five");
+
+?> 
