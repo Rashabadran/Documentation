40384872b3186edf34a9dda2546ae0c77718d4a8
Rasha <badranrasha685@gmail.com>
2023-02-19 13:43:49 +0200

    step8b

---
+++ 03_Exercises/06_PHP/basic-01/step8.php
@@ -0,0 +1,12 @@
+<?php
+
+$grocerry=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+$length=count($grocerry);
+echo "Hello Sir, do you have ",$grocerry[0]," , ",$grocerry[1]," , ",$grocerry[2], " ? Also if you sell fruits can I find a ", $grocerry[5],"<br>";
+
+$eggs=array('balade','mazere3');
+$milk=array("fresh","taanayel");
+$water_pack=array("Tanoureen","Reem");
+echo"Hey Sir, Please I need 1 pack of ",$eggs[0]," eggs and 3 ",$water_pack[1]," Water Pack";
+
+?>
--- 03_Exercises/06_PHP/basic-01/step8a.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$grocerry=array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
-$length=count($grocerry);
-echo "Hello Sir, do you have ",$grocerry[0]," , ",$grocerry[1]," , ",$grocerry[2], " ? Also if you sell fruits can I find a ", $grocerry[5];
-?>
