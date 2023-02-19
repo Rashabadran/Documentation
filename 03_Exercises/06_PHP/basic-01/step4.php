9517951e339217b470c613df4de2975a46a776cb
Rasha <badranrasha685@gmail.com>
2023-02-19 12:45:29 +0200

    step4

---
+++ 03_Exercises/06_PHP/basic-01/step4.php
@@ -0,0 +1,9 @@
+
+<?php
+
+$name="Gaby";
+$position="tech consultant";
+$github_url="https://github.com/gk-git";
+echo"'Hello, I'm" ,$name, " ,I'm a " ,$position," please check my github link '",$github_url,"'";
+
+?>
